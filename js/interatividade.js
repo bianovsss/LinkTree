17b9db4c11071f3c538bec7b67603187daf89c7e
bianovsss <bianovss@gmail.com>
2026-03-31 22:17:44 -0300

    Introdução JS

---
+++ js/interatividade.js
@@ -0,0 +1,38 @@
+$(function(){
+ 
+ 
+//ENTRADA
+ 
+var botoes = $(".botao");
+var titulo = $("h1");
+var subtitulo = $("#subtitulo");
+var icones = $("svg");
+var fundo = $("body");
+ 
+//PROCESSAMENTO
+
+titulo.click(()=>{
+    
+    botoes.css("background-color","white");
+
+});
+
+titulo.dblclick(()=>{
+
+    fundo.html("<h1> Alterado pelo JS </h1>");
+
+});
+
+subtitulo.click(()=>{
+
+    icones.css("fill, black");
+
+});
+    
+
+    //SAIDA
+ 
+ 
+ 
+ 
+});
